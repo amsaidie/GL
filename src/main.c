ffdaae0fda616c1fdef7dcaf2b6ec650369aa2f5
aman-zakir msaidie <aman-zakir.msaidie@ge1.iut-cachan.u-psud.fr>
2018-11-22 15:15:52 +0100

    Partie 1

--- src/main.c
+++ src/main.c
@@ -2,24 +2,22 @@
 //
 
 #include "my_header.h"
-
+#define SEUIL 5
 
 int main(void)
 {
 unsigned char bin[10] =
 {0,1,2,3,4,5,6,7,8,9};
-int i;
-int SEUIL = 5;
-			
-			for(i=0;i<4;i=i++)
+int i=0;
+
+for(i=0;i<10;i=i+1)
 {
-	if(bin>SEUIL) 
+	if(bin[i]>SEUIL) 
 	{
 		bin[i]=1;
 	}
 			   
-	else  
-	{
+	else if(bin[i]<SEUIL) {
 		bin[i]=0;
 	}
 }
