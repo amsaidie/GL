90d90a96fbce3d300a732cf7fde12a2003812e6b
aman-zakir msaidie <aman-zakir.msaidie@ge1.iut-cachan.u-psud.fr>
2018-11-29 12:51:13 +0100

    partie 1 terminée

--- src/main.c
+++ src/main.c
@@ -3,25 +3,18 @@
 #include "my_header.h"
 #include "TP_lib_suite.h"
 
-/*int partie1(void);
-
-int main(void)
-{
-	partie1();
-    return 0; */
-
 int main(void)
 {
 	unsigned char joueur[6] = {3, 4, 33, 12, 42, 9};
-	unsigned char gagnant[6] = {12, 40, 36, 5, 9, 27};
+	unsigned char gagnant[6] = {30, 20, 4, 9, 28, 47};
 	int i=0, j=0;
 	int bons_num=0;
 	
-	for(i=0; i<1; i++)
+	for(i=0; i<6; i++)
 	{
 		for(j=0; j<6; j++)
 		{
-			if(joueur[i]==joueur[j])
+			if(joueur[i]==gagnant[j])
 				bons_num=bons_num+1;
 			}
 	}
