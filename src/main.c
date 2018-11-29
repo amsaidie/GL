86cbc9acc2e68c7c669bd76a0b6c3f795f38a5dc
aman-zakir msaidie <aman-zakir.msaidie@ge1.iut-cachan.u-psud.fr>
2018-11-29 14:30:17 +0100

    partie4 terminée

--- src/main.c
+++ src/main.c
@@ -3,21 +3,35 @@
 #include "my_header.h"
 #include "TP_lib_suite.h"
 
+
 int main(void)
 {
 	unsigned char joueur[6] = {3, 4, 33, 12, 42, 9};
-	unsigned char gagnant[6] = {30, 20, 4, 9, 28, 47};
+	unsigned char gagnant[6] = {0, 0, 0, 0, 0, 0};
 	int i=0, j=0;
 	int bons_num=0;
+	
+///////////////////////////////////////////////////////////////////////
+	
+	initialiserTirage();
+	
+	for(i=0; i<6; i++)
+	{
+		gagnant[i]=tirerNumero();
+	}
+
+
 	
 	for(i=0; i<6; i++)
 	{
 		for(j=0; j<6; j++)
 		{
 			if(joueur[i]==gagnant[j])
+
 				bons_num=bons_num+1;
 			}
 	}
 	
     return 0;
 }
+
