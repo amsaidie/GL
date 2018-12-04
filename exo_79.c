92106ec177df2ce1c61cc36e93ba5b9207ca38be
aman-zakir msaidie <aman-zakir.msaidie@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:58:39 +0100

    Exo_79 en cours

---
+++ exo_79.c
@@ -0,0 +1,22 @@
+//  Copyright (c) 2018 Antoine Tran Tan
+
+#include "my_header.h"
+#include "TP_lib_suite.h"
+
+
+int main(void)
+{
+	char phrase[100];
+	char prenom[100];
+	char nom[100];
+	unsigned long int annee;
+					   
+///////////////////////////////////////////////////////////////////////
+	
+	sscanf(phrase, "%*s %*s %s %s %*s %*s %*s %*s %*s %lu", nom, prenom, &annee);
+	
+	sprintf("Je m'appelle Msaidie Aman et je suis ne en 2000", phrase);
+	
+	return 0;
+
+}
