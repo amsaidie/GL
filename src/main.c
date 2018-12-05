5f035bf9946597b4be0b79619a314840cbea81d0
amsaidie <aman-zakir.msaidie@u-psud.fr>
2018-12-05 23:03:31 +0100

    Affichage des sommets et du nom de la figure sur un seul Popup

--- src/main.c
+++ src/main.c
@@ -10,22 +10,24 @@ int main(void)
 {
 	char ligne1[80]="Nom: Pentagone";  //sprintf(ligne1, "Nom: Pentagone")
 	char ligne2[80]="nb_sommets: 5";
-	//char ligne3[80]="coordonnees: 10 20 30 20 30 40 43 53 35 65";
+	char ligne3[80]="coordonnees: 10 20 30 20 30 40 43 53 35 65";
 	char Nom[80];
+	char Message[80];
 	unsigned int nb_sommets;
-	//char coordonneesx[4];
-	//char coordonneesy[4];
+	char coordonneesx[5];
+	char coordonneesy[5];
 					   
 ///////////////////////////////////////////////////////////////////////
 	sscanf(ligne1, "%*s %s", Nom);
 	sscanf(ligne2, "%*s %u", &nb_sommets);
 	//sscanf(ligne3, "%*s %s %s %s %s %s", coordonnees);	
 		
-	sprintf(ligne1, "C'est un %s", Nom);	
-	sprintf(ligne2, "avec %u sommets", nb_sommets);
-	
-	MessagePopup("Voici le type de ploygone", ligne1);
-	MessagePopup("Voici le nombre de sommet", ligne2);
+	//sprintf(ligne1, "C'est un %s", Nom);
+	//sprintf(ligne2, "avec %u sommets", nb_sommets);
+	sprintf(Message, "C'est un %s avec %u sommets",Nom, nb_sommets);
+	//MessagePopup("Voici le type de ploygone", ligne1);
+	//MessagePopup("Voici le nombre de sommet", ligne2);
+	MessagePopup("Caracteristiques", Message);
 																
 	return 0;
 
